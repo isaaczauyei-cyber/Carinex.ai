c254f43ba22bf889943a8c64aa790bffcf0a2190
isaaczauyei-cyber <isaaczauyei@gmail.com>
2026-08-30 11:35:32 +0100

    Create page.tsx

---
+++ app/faq/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import FAQ from "@/components/FAQ";
+
+export default function FAQPage() {
+  return (
+    <main>
+      <Navbar />
+      <FAQ />
+      <Footer />
+    </main>
+  );
+}
